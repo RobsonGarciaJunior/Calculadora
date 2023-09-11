ac653d2279ed333faa38e168fa7d11830c4c4ad7
Adr10 <64582247+Adr10@users.noreply.github.com>
2023-09-11 16:56:54 +0200

    Primer Commit Prueba

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CalculadoraActividad1_Robson_1EVA"
+include(":app")
+ 
